bb256753c46450b93324090225278d9cf87ac129
AnaGCastro <testeanateste0@gmail.com>
2025-06-04 12:07:23 -0300

    Configuracao do projeto criado

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+      <span className="text-xl font-semibold text-gray-800 dark:text-gray-100">
+        Texto
+      </span>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+variants:{
+
+},
+
+  plugins: [],
+}
+
